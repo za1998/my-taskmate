b6b9682e8e861f635c02e865d3bb189d38195b4a
za1998 <muhammadrezaindo23@gmail.com>
2025-08-02 16:53:10 +0700

    app

--- src/pages/ChatAI.jsx
+++ src/pages/ChatAI.jsx
@@ -15,7 +15,8 @@ export default function ChatAI() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      //const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://j0l5sptb.up.railway.app/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
